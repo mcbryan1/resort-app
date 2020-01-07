c18a7b535856c75b164ebdc80f733f91d2c6b73f
McBryan <solomonmcbryan@gmail.com>
2020-01-07 13:44:24 -0800

    instaled react icons, created navbar component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,13 @@ import Error from "./pages/Error"
 
 import { Route, Switch } from 'react-router-dom'
 
+import Navbar from "./components/Navbar"
+
+
 function App() {
   return (
     <div>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms" component={Rooms} />
