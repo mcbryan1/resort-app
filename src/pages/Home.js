7295ea2f71388ab7b489f396d17ed961db3dcf7a
McBryan <solomonmcbryan@gmail.com>
2020-01-07 16:09:22 -0800

    added banner, createed room components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
-        <div>
-            hello from home page
-        </div>
+        <Hero>
+            <Banner title="luxirious Rooms"
+                subtitle="deluxe rooms starting at $299">
+                <Link to="/rooms" className="btn-primary">
+                    our rooms
+                </Link>
+            </Banner>
+        </Hero>
     )
+
 }
