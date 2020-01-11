40b7c6b0aad29c3e506daa97920376f3dbd8ec8c
McBryan <solomonmcbryan@gmail.com>
2020-01-11 08:40:28 -0800

    ommitted loading

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react'
 import { RoomContext } from "../context"
+
 import Room from "./Room"
 import Title from './Title'
 
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext
     render() {
-        const { featuredRooms: rooms } = this.context
-        console.log(rooms);
+        let { featuredRooms: rooms } = this.context;
+        rooms = rooms.map(room => {
+            return <Room key={room.id} room={room} />
+        })
+
 
 
         return (
-            <div>
-                hello from featured rooms
-                <Room />
-            </div>
+            <section className="featured-rooms">
+                <Title title="featured rooms" />
+                <div classNAme="featured-rooms-center">
+                    {rooms}
+                </div>
+            </section>
         )
     }
 }
---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Gif from '../images/gif/loading-arrow.gif'
+
+export default function Loading() {
+    return (
+        <div>
+            <Gif />
+        </div>
+    )
+}
